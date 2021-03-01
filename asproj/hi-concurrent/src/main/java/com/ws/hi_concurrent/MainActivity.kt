8ef70332fe191280110a0fb838cc3e88191f07ef
wangshun <wangshun3@jd.com>
2021-03-01 22:59:14 +0800

    add file

---
+++ asproj/hi-concurrent/src/main/java/com/ws/hi_concurrent/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ws.hi_concurrent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        ConcurrentTest.main(this)
+    }
+}
