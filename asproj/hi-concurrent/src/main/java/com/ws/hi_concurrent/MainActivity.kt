c4d59a7b76ba043dab0d542f84808e6eed46fafb
wangshun <wangshun3@jd.com>
2021-03-03 23:14:22 +0800

    普通方法适配协程

--- asproj/hi-concurrent/src/main/java/com/ws/hi_concurrent/MainActivity.kt
+++ asproj/hi-concurrent/src/main/java/com/ws/hi_concurrent/MainActivity.kt
@@ -3,7 +3,12 @@ package com.ws.hi_concurrent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.whenCreated
+import androidx.lifecycle.whenResumed
+import androidx.lifecycle.whenStarted
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlin.coroutines.Continuation
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +27,26 @@ class MainActivity : AppCompatActivity() {
             Log.e("MainActivity", result.getOrNull())
         }
         CoroutineScene2_decompiled.request2(callback)
+
+        lifecycleScope.launch {
+            val content = CoroutineScene3.parseAssetsFile(assets, "config.json")
+            Log.e("MainActivity", content)
+        }
+
+        //但我们的宿主的生命周期至少是onCreate的时候才会启动
+        lifecycleScope.launchWhenCreated {
+            whenCreated {
+                //这里的代码只有宿主的生命周期为onCreate的时候才会执行，否则都是暂停的
+            }
+
+            whenResumed {
+
+            }
+
+            whenStarted {
+
+            }
+        }
 
     }
 }
